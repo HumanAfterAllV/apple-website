eb5be1798cb8170d62f8ec3003994dbeba4b7320
V <motocycle9719@hotmail.com>
2024-11-04 09:16:46 -0600

    add(hero-page): animations

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
+
+
+
+export default function App() {
+  return (
+    <main className="bg-black">
+        <Navbar />
+        <Hero />
+
+    </main>
+  )
+}
+
