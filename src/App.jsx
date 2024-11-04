cff69e42f5799a77f25728126386661cc5fdd42f
V <motocycle9719@hotmail.com>
2024-11-04 11:14:41 -0600

    add(3D-model): iPhone 15 Three.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
-
-
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 
 export default function App() {
   return (
     <main className="bg-black">
         <Navbar />
         <Hero />
-
+        <Highlights />
+        <Model />
     </main>
   )
 }
